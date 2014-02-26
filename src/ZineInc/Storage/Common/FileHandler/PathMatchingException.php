53c0f1d498bdabc5145232b7b2cf1c13a49da4a8
psliwa <peter.pl7@gmail.com>
2014-02-26 22:30:57 +0100

    fix PathMatchingEx

--- src/ZineInc/Storage/Common/FileHandler/PathMatchingException.php
+++ src/ZineInc/Storage/Common/FileHandler/PathMatchingException.php
@@ -2,8 +2,15 @@
 
 namespace ZineInc\Storage\Common\FileHandler;
 
+use Exception;
+use ZineInc\Storage\Common\ErrorCodes;
 use ZineInc\Storage\Common\StorageException;
 
 class PathMatchingException extends \Exception implements StorageException
 {
+    public function __construct($message = "", $code = ErrorCodes::INVALID_CHECKSUM, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
 }
