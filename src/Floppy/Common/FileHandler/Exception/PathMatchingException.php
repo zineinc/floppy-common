b098fe3ed6add149e7ee98719c49ad19f0aee198
psliwa <peter.pl7@gmail.com>
2014-04-17 23:36:04 +0200

    remove references to ErrorCodes class

--- src/Floppy/Common/FileHandler/Exception/PathMatchingException.php
+++ src/Floppy/Common/FileHandler/Exception/PathMatchingException.php
@@ -3,14 +3,8 @@
 namespace Floppy\Common\FileHandler\Exception;
 
 use Exception;
-use Floppy\Common\ErrorCodes;
 use Floppy\Common\Exception\StorageException;
 
 class PathMatchingException extends \Exception implements StorageException
 {
-    public function __construct($message = "", $code = ErrorCodes::INVALID_CHECKSUM, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
 }
--- src/Floppy/Common/Stream/Exception/IOException.php
+++ src/Floppy/Common/Stream/Exception/IOException.php
@@ -3,13 +3,8 @@
 namespace Floppy\Common\Stream\Exception;
 
 use Exception;
-use Floppy\Common\ErrorCodes;
 use Floppy\Common\Exception\StorageException;
 
 class IOException extends Exception implements StorageException
 {
-    public function __construct($message = null, Exception $previous = null)
-    {
-        parent::__construct($message, ErrorCodes::IO, $previous);
-    }
 }
